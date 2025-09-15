10fd5a314aa75db62456ca2ebfb50d07a84f8ebe
Dhruv Singh <dhruvsingh7077@gmail.com>
2025-09-15 18:53:03 +0530

    portfolio completed: my portfolio

---
+++ src/components/About/About.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
+import { Typewriter } from "react-simple-typewriter";
+import Tilt from "react-parallax-tilt";
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side*/}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greetings*/}
+          <h1 className="text-3xl sm:text-6xl font-bold text-white mb-2 leading-tight">
+            HI I AM
+          </h1>
+          {/* Name*/}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Dhruv Singh
+          </h2>
+          {/* Skills heading with typing effect*/}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold  mb-4  text-[#8245ec] leading-tight">
+            <span className="text-white">i am a </span>
+            <span className="text-[#8245ec]">
+              <Typewriter
+                words={["Full Stack Devloper", "UI/UX Devloper", "Coder"]}
+                loop={true}
+                cursor
+                cursorStyle="|"
+                typeSpeed={100}
+                deleteSpeed={50}
+                delaySpeed={2000}
+              />
+            </span>
+          </h3>
+          {/* about paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a passionate and dedicated Full Stack Developer with a knack
+            for crafting seamless web experiences. With a strong foundation in
+            both front-end and back-end technologies.
+          </p>
+          <a
+            href="My Resume (1).pdf" // place your CV inside the public folder
+            download="Dhruv_Singh_CV.pdf"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <img
+            src={""}
+            alt="ds"
+            className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { useState, useEffect } from "react";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // ckeck scroll and change navbar background
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  //Smooth scroll function
+  const handleMenuItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+    setIsOpen(false);
+
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "experience", label: "Experience" },
+    { id: "work", label: "Work" },
+    { id: "education", label: "Education" },
+    { id: "contact", label: "Contact" },
+  ];
+  return (
+    <nav
+      className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] lg:px-[20vw] ${
+        isScrolled
+          ? "bg-[#050414] bg-opacity-50 background-blur-md shadow-md"
+          : "bg-transparent"
+      }`}
+    >
+      <div className="text-white py-5 flex justify-between items -center">
+        {/* logo */}
+        <div className="text-lg font-semibold cursor-pointer">
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Dhruv</span>
+          <span className="text-[#8245ec]">/</span>
+          <span className="text-white">Singh</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
+        {/* Desktop Menu */}
+        <ul className=" hidden md:flex space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li
+              key={item.id}
+              className={`cursor-pointer hover:text-[#8245ec] ${
+                activeSection === item.id ? "text-[#8245ec]" : ""
+              }`}
+            >
+              <button onClick={() => handleMenuItemClick(item.id)}>
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+        {/* social media icons*/}
+        <div className="hidden md:flex space-x-4">
+          <a
+            href="https://github.com/DhruvSingh7077"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/dhruv-singh70/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
+        {/* moblie Menu Icons */}
+        <div className="md:hidden">
+          {isOpen ? (
+            <FiX
+              className="text-3xl text-[#8245ec] cursor pointed"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FiMenu
+              className="text-3xl  text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
+        </div>
+      </div>
+      {/* Mobile menu Items */}
+      {isOpen && (
+        <div className="absolute top-16 left-1/2 -translate-x-1/2 w-4/5 bg-gray-800/60 backdrop-blur-md z-50 rounded-xl shadow-lg">
+          <ul className="flex flex-col items-center space-y-4 py-6 text-gray-300">
+            {menuItems.map((item) => (
+              <li
+                key={item.id}
+                className={`cursor-pointer hover:text-white
+                    ${activeSection === item.id ? " text-[#8245ec]" : ""}`}
+              >
+                <button onClick={() => handleMenuItemClick(item.id)}>
+                  {item.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
